ad75e07c7a7b39eb38bf803b3efde74b4ac8e077
yaoliangdong <2336965371@qq.com>
2017-02-18 22:27:09 +0800

    目录结构和配置文件

---
+++ app/startup/redis.sh
@@ -0,0 +1,7 @@
+
+../software/redis-group/redis-6379/src/redis-server ../software/redis-group/redis-6379/redis.conf
+../software/redis-group/redis-6379/src/redis-server ../software/redis-group/redis-6479/redis.conf
+../software/redis-group/redis-6379/src/redis-server ../software/redis-group/redis-6579/redis.conf
+../software/redis-group/redis-6379/src/redis-server ../software/redis-group/redis-6679/redis.conf
+../software/redis-group/redis-6379/src/redis-server ../software/redis-group/redis-6779/redis.conf
+../software/redis-group/redis-6379/src/redis-server ../software/redis-group/redis-6879/redis.conf
---
+++ app/startup/zookeeper.sh
@@ -0,0 +1,5 @@
+../software/zookeeper-group/zookeeper-2181/bin/zkServer.sh start
+../software/zookeeper-group/zookeeper-2182/bin/zkServer.sh start
+../software/zookeeper-group/zookeeper-2183/bin/zkServer.sh start
+../software/zookeeper-group/zookeeper-2184/bin/zkServer.sh start
+../software/zookeeper-group/zookeeper-2185/bin/zkServer.sh start
